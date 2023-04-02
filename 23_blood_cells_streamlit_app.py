8818686495e96a9e13f8868a7ce8524e0d20785a
RoadieHouse <120097990+RoadieHouse@users.noreply.github.com>
2023-04-02 21:27:34 +0200

    Update 23_blood_cells_streamlit_app.py

--- 23_blood_cells_streamlit_app.py
+++ 23_blood_cells_streamlit_app.py
@@ -252,9 +252,9 @@ if selected == 'E.D.A.':
         col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
         col1.image(cell_01, use_column_width=True, caption = 'BAS, Munich')
         col2.image(cell_02, use_column_width=True, caption = 'EOS, Munich')
-        col3.image(cell_03, use_column_width=True, caption = "ERY,\n Munich")
-        col4.image(cell_04, use_column_width=True, caption = 'IG,       Munich')
-        col5.image(cell_05, use_column_width=True, caption = "LT,\n Munich")
+        col3.image(cell_03, use_column_width=True, caption = "ERY, Munich")
+        col4.image(cell_04, use_column_width=True, caption = 'IG, Munich')
+        col5.image(cell_05, use_column_width=True, caption = "LT, Munich")
         col6.image(cell_06, use_column_width=True, caption = 'MON, Munich')
         col7.image(cell_07, use_column_width=True, caption = 'NEU, Munich')
 
@@ -274,7 +274,7 @@ if selected == 'E.D.A.':
         col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
         col1.image(cell_17, use_column_width=True, caption = 'BAS, Raabin')
         col2.image(cell_18, use_column_width=True, caption = 'EOS, Raabin')
-        col5.image(cell_21, use_column_width=True, caption = "LT,\n Raabin")
+        col5.image(cell_21, use_column_width=True, caption = "LT, Raabin")
         col6.image(cell_22, use_column_width=True, caption = 'MON, Raabin')
         col7.image(cell_23, use_column_width=True, caption = 'NEU, Raabin')
 #------------------------------------------------------------------------------------------------------------------------------------------    
@@ -422,9 +422,9 @@ except Exception as e:
 # Preprocess image
 def preprocess_image(image):
     if image is not None:
-        image = image.resize(IMG_SIZE)
+        image = ImageOps.fit(image, IMG_SIZE, Image.BICUBIC)
         image = tf.keras.preprocessing.image.img_to_array(image)
-        image = tf.keras.applications.resnet_v2.preprocess_input(image)
+        #image = tf.keras.applications.resnet_v2.preprocess_input(image)
         return image
     
 # Function to make predictions
