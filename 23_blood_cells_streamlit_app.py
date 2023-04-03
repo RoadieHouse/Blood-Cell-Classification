4c2c7ddc3e95ff2f2a9c98a90faa53e83743f075
AmrithaPrasneh <125680019+AmrithaPrasneh@users.noreply.github.com>
2023-04-03 22:49:14 +0200

    Update 23_blood_cells_streamlit_app.py

--- 23_blood_cells_streamlit_app.py
+++ 23_blood_cells_streamlit_app.py
@@ -482,8 +482,8 @@ if selected == 'Prediction':
             directory += selected_class
             FOLDER_PATH = os.path.join(os.path.dirname(__file__), "images/basophil")
 
-            st.write(listdir(FOLDER_PATH))
-            image_file = listdir(FOLDER_PATH)[0]
+            dict = listdir(FOLDER_PATH)
+            st.write(list(dict.items()))
 
         if image_file is not None:
             image = open_image(image_file)
