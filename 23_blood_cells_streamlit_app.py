e98bbb392eacb37d6e1b8c1dd3c39f9112872917
AmrithaPrasneh <125680019+AmrithaPrasneh@users.noreply.github.com>
2023-04-03 18:28:48 +0200

    Update 23_blood_cells_streamlit_app.py

--- 23_blood_cells_streamlit_app.py
+++ 23_blood_cells_streamlit_app.py
@@ -477,6 +477,7 @@ if selected == 'Prediction':
 
         with r_col:
             selected_class = st.selectbox("Select a class:", ["Please make selection",*CLASS_LABELS])
+            list_images(directory, selected_class)
             #directory = 'images/'
             #selected_file = '20190531_111246_0.jpg' #list_images(directory, selected_class)
             #image_file = directory + selected_class + '/' + selected_file
